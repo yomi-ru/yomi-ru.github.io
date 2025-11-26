07792f376b437afe9248f992eabf8d132730e389
yomi-ru <yomiru.system@gmail.com>
2025-11-26 16:07:25 +0900

    added 16 code

--- mainscript.js
+++ mainscript.js
@@ -24,3 +24,5 @@ window.addEventListener('scroll', () => {
         header.classList.remove('fixed');
     }
 });
+
+// 594f4d4952552070726f6a6563740a6d61646520627920e69d89e69cace79bb4e7b6990ae89197e4bd9ce6a8a9e381afe4bf9de8adb7e38195e3828ce381a6e38184e381bee38199e38082
