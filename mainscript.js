08031d85fd99fbd54fcdad479a1f3c85c47e005b
yomi-ru <yomiru.system@gmail.com>
2025-12-10 11:25:26 +0900

    rebacked button

--- mainscript.js
+++ mainscript.js
@@ -22,4 +22,15 @@ window.addEventListener('scroll', () => {
     }
 });
 
+function showNextButton() {
+    const checkbox = document.getElementById("initialButton");
+    const button = document.getElementById("nextButton");
+
+    if (checkbox.checked) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
+
 // 594f4d4952552070726f6a6563740a6d61646520627920e69d89e69cace79bb4e7b6990ae89197e4bd9ce6a8a9e381afe4bf9de8adb7e38195e3828ce381a6e38184e381bee38199e38082
