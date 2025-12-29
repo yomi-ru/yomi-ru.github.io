cdd8ddd14448aba60602f3e3b89e7849e034beb1
yomi-ru <yomiru.system@gmail.com>
2025-12-29 21:55:21 +0900

    new data uploaded

--- YOMIRU_student/student-main/student-study/dashboard/dashboard.js
+++ YOMIRU_student/student-main/student-study/dashboard/dashboard.js
@@ -2,26 +2,47 @@ const LEVELS = [
     {
       id: 1,
       name: "1桁",
-      desc: "まずは英語読み上げ算を学んでみましょう。",
       href: "../levels/1digit/1digit.html"
     },
     {
       id: 2,
       name: "2桁",
-      desc: "1桁が簡単すぎるように感じたらここに進みましょう。",
       href: "../levels/2digit/2digit.html"
     },
     {
       id: 3,
       name: "3桁",
-      desc: "初級の問題の中でも難しめの問題を用意しました。",
       href: "../levels/3digit/3digit.html"
+    },
+    {
+      id: 36,
+      name: "3-6桁",
+      href: "../levels/3to6digit/3to6digit.html"
+    },
+    {
+      id: 47,
+      name: "4-7桁",
+      href: "../levels/4to7digit/4to7digit.html"
+    },
+    {
+      id: 58,
+      name: "5-8桁",
+      href: "../levels/5to8digit/5to8digit.html"
+    },
+    {
+      id: 711,
+      name: "7-11桁",
+      href: "../levels/7to11digit/7to11digit.html"
     },
     {
       id: 713,
       name: "7-13桁",
-      desc: "全国大会上位レベルの読み上げ算。",
       href: "../levels/7to13digit/7to13digit.html"
+    },
+    {
+      id: 913,
+      name: "9-13桁",
+      href: "../levels/9to13digit/9to13digit.html"
     },
   ];
   
@@ -44,9 +65,6 @@ const LEVELS = [
              style="height:auto; padding:20px; cursor:pointer;"
              onclick="location.href='${level.href}'">
           <h3 style="padding-top:10px;">${level.name}</h3>
-          <p style="padding-top:10px; padding-bottom:0;">
-            ${level.desc}
-          </p>
           <p style="padding-top:18px; padding-bottom:10px; font-weight:bold;">
             ▶ 開く
           </p>
